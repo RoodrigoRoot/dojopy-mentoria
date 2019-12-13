aa2bd48d8e4b4197a88586f7a5fcdcf024360640
roodrigoroot <roodrigoroot@gmail.com>
2019-12-12 21:15:43 -0600

    Conociendo funciones

---
+++ index.js
@@ -0,0 +1,15 @@
+//alert('') //Funcion que muestra una ventana en el navegador
+
+function nombre(){
+
+        alert("Rodrigo")
+        console.log('Rodrigo')
+}
+
+/*
+Tarea
+Event
+Mouse (Cuando el usuario pase el mouse )
+Caja de texto (Rodrigo)
+Click
+*/
